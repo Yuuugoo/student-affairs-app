1d50666e5226ea62d424d89908f810e34b63e3fc
Yuuugoo <mdtcristoles2021@plm.edu.ph>
2024-05-05 21:18:44 +0800

    Login Reverted

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,6 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->default()
+            ->login()
             ->colors([
                 'primary' => '#c6ab5d',
             ])
@@ -62,10 +63,10 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            // ->authMiddleware([
-            //     Authenticate::class,
-            //     'admin.email',
-            // ])
+            ->authMiddleware([
+                Authenticate::class,
+                'admin.email',
+            ])
             ->plugin(
                 FilamentFullCalendarPlugin::make()
                     ->config([])
--- app/Providers/Filament/StudentOfficerPanelProvider.php
+++ app/Providers/Filament/StudentOfficerPanelProvider.php
@@ -33,7 +33,7 @@ class StudentOfficerPanelProvider extends PanelProvider
         return $panel
             // ->brandLogo(asset('images/plm-logo-header.svg'))
             // ->brandLogoHeight('3rem')
-            // ->login()
+            ->login()
             ->registration()
             ->profile(EditProfile::class)
             ->id('studentOfficer')
@@ -62,15 +62,16 @@ class StudentOfficerPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            // ->authMiddleware([
-            //     Authenticate::class,
+            ->authMiddleware([
+                Authenticate::class,
                 
 
-            // ])
+            ])
             ->plugin(
                 FilamentFullCalendarPlugin::make()
                     ->config([])
                     ->plugins(['dayGrid', 'timeGrid'])
+                    ->selectable()
                     
             );
     }
